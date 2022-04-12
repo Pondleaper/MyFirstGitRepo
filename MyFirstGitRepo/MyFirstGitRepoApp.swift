8f712243ab01e97b31061f7d1ab967dae668ed8d
Pondleaper <richardkparker@gmail.com>
2022-04-12 09:01:03 -0400

    Initial Commit

---
+++ MyFirstGitRepo/MyFirstGitRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGitRepoApp.swift
+//  MyFirstGitRepo
+//
+//  Created by Richs Old Mac on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstGitRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
